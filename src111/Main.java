7b9a3aed095c6fd03487da5c39bc75776fa9555b
MatheusGuion <81374933+MatheusGuion@users.noreply.github.com>
2026-03-31 20:08:16 +0000

    Comentários da Main

--- src111/Main.java
+++ src111/Main.java
@@ -10,6 +10,7 @@ import java.io.EOFException;
 
 public class Main {
     public static void main(String[] args) {
+        //verificação de quantidade mínima de argumentos
         if (args.length < 3) {
             System.out.println("Uso incorreto! Utilize:");
             System.out.println("Para comprimir: java -jar huffman.jar c <arquivo_original> <arquivo_comprimido>");
@@ -17,6 +18,7 @@ public class Main {
             return;
         }
 
+        //lê os argumentos
         String comando = args[0];
         String arquivoEntrada = args[1];
         String arquivoSaida = args[2];
@@ -93,6 +95,7 @@ public class Main {
         }
     }
 
+    //cria um vetor de 256, e lê a frequência de cada letra
     public static int[] contarFrequencias(String texto){
         int[] frequencias = new int[256];
         for(int i = 0; i < texto.length(); i++){
@@ -101,8 +104,11 @@ public class Main {
         return frequencias;
     }
 
+    //constroi a árvore de huffman de cima para baixo, seguindo o MinHeap
     public static No construirArvore(int[] frequencias) {
         MinHeap heap = new MinHeap();
+        
+        //Cria uma folha para cada caractere que aparece pelo menos uma vez
         for (int i = 0; i < frequencias.length; i++) {
             if (frequencias[i] > 0) {
                 heap.inserir(new No((char) i, frequencias[i]));
@@ -113,27 +119,36 @@ public class Main {
         System.out.println("\nETAPA 2: Min-Heap Inicial (Vetor)");
         heap.imprimir();
 
+        //Combina os nós de menor frequêcia repetidamente até sobrar a raiz
         while (heap.tamanho() > 1) {
             No noEsquerda = heap.remover();
             No noDireita = heap.remover();
+
+            //Cria um nó pai com a soma das frequências
             No pai = new No('\0', noEsquerda.frequencia + noDireita.frequencia);
             pai.esquerda = noEsquerda;
             pai.direita = noDireita;
             heap.inserir(pai);
         }
+        //último nó é a raiz da árvore de huffman
         return heap.remover();
     }
 
+    //Percorre a árvore recursivamente para dar os códigos binários
     public static void gerarCodigos(No raiz, String codigo, String[] tabela) {
         if (raiz == null) return;
+        //Se for um nó folha, chegamos a um caractere. Salvamos o código
         if (raiz.esquerda == null && raiz.direita == null) {
             tabela[raiz.caractere] = codigo;
             return;
         }
+
+        //Chamadas recursivas descendo a árvore
         gerarCodigos(raiz.esquerda, codigo + "0", tabela);
         gerarCodigos(raiz.direita, codigo + "1", tabela);
     }
 
+    //Converte o texto original para um string de 0s e 1s
     public static String codificar(String texto, String[] tabela) {
         StringBuilder resultado = new StringBuilder();
         for (int i = 0; i < texto.length(); i++) {
@@ -142,26 +157,30 @@ public class Main {
         return resultado.toString();
     }
 
+    //Navega a árvore de Huffman seguindo os bits codificados, para achar seu significado original
     public static String decodificar(String codigo, No raiz) {
         StringBuilder resultado = new StringBuilder();
         No atual = raiz;
         for (int i = 0; i < codigo.length(); i++) {
             char bit = codigo.charAt(i);
             
+            //Desce à esquerda se o bit for 0, ou direita se o bit for 1
             if (bit == '0') {
                 atual = atual.esquerda;
             } else {
                 atual = atual.direita;
             }
 
+            //Se chegou um nó na folha, encontramos o caractere
             if (atual.esquerda == null && atual.direita == null) {
                 resultado.append(atual.caractere);
-                atual = raiz; 
+                atual = raiz;//Volta à raiz para buscar o próximo caractere
             }
         }
         return resultado.toString();
     }
 
+    //Lê o arquivo de texto inteiro em memória
     public static String lerArquivo(String caminho) {
         StringBuilder conteudo = new StringBuilder();
         try (BufferedReader br = new BufferedReader(new FileReader(caminho))) {
@@ -175,28 +194,35 @@ public class Main {
         return conteudo.toString();
     }
 
+    //Salva informações necessárias para a descompressão
     public static void salvarArquivo(String caminho, int[] frequencias, String textoCodificado) {
+        //Usa JavaOutputStream para escrever Java de forma binária
         try (DataOutputStream dos = new DataOutputStream(new FileOutputStream(caminho))) {
+            //Grava o cabeçalho - vetor de frequências
             for (int i = 0; i < 256; i++) {
                 dos.writeInt(frequencias[i]);
             }
             
+            //Grava a quantidade exata de bits úteis
             dos.writeInt(textoCodificado.length());
 
+            //Agrupa os 0s e 1s em bits reais ou invés de Strings
             int buffer = 0;
             int contBits = 0;
             for (int i = 0; i < textoCodificado.length(); i++) {
-                buffer <<= 1;
+                buffer <<= 1;//Empurra os bits do buffer, uma casa para a direita
                 if (textoCodificado.charAt(i) == '1') {
-                    buffer |= 1;
+                    buffer |= 1;//Se o bit atual for 1, liga ao último bit do buffer
                 }
                 contBits++;
+                //Quando completar 8 bits, escreve o byte no arquivo e zera os contadores
                 if (contBits == 8) {
                     dos.writeByte(buffer);
                     buffer = 0;
                     contBits = 0;
                 }
             }
+            //Caso bits que não completaram 8 bits, precisamos completar com 0s a direita
             if (contBits > 0) {
                 buffer <<= (8 - contBits);
                 dos.writeByte(buffer);
@@ -207,20 +233,27 @@ public class Main {
         }
     }
 
+    //Lê o arquivo binário, recria a árvore, extrai os bits e restaura o arquivo original
     public static void descomprimirArquivo(String arquivoEntrada, String arquivoSaida) {
         try (DataInputStream dis = new DataInputStream(new FileInputStream(arquivoEntrada))) {
+            //Lê o vetor de frequências
             int[] frequencias = new int[256];
             for (int i = 0; i < 256; i++) {
                 frequencias[i] = dis.readInt();
             }
 
+            //Lê o tamanho exato de bits originais comprimidos
             int tamanhoRealBits = dis.readInt();
+
+            //Reconstrói a mesma árvore de Huffman usada no início
             No raiz = construirArvore(frequencias);
 
             StringBuilder bitsCodificados = new StringBuilder();
+            //Lê todos os bits e converte de volta para string de 0s e 1s
             try {
-                while (true) {
+                while (true) {//loop infinito q acabará por conta da exceção
                     byte b = dis.readByte();
+                    //Extrai bit a bit do byte lido
                     for (int i = 7; i >= 0; i--) {
                         bitsCodificados.append((b >> i) & 1);
                     }
@@ -229,9 +262,13 @@ public class Main {
                 //fim do arquivo
             }
 
+            //Corta os bits extras de preenchimento
             String bitsValidos = bitsCodificados.substring(0, tamanhoRealBits);
+
+            //Decodifica a string binária validada pela árvore
             String textoDecodificado = decodificar(bitsValidos, raiz);
 
+            //Salva o texto restaurado em um arquivo normal
             try (FileWriter fw = new FileWriter(arquivoSaida)) {
                 fw.write(textoDecodificado);
             }
